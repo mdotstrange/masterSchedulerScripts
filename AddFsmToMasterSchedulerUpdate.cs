faf10f3cb897247995725f49f833957b084aa93e
mdotstrange <mdotstrange@gmail.com>
2019-07-06 18:52:20 -0700

    Add files via upload

---
+++ AddFsmToMasterSchedulerUpdate.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+
+	[ActionCategory(ActionCategory.GameObject)]
+	public class AddFsmToMasterSchedulerUpdate : FsmStateAction
+	{
+        public FsmString QueueName;
+
+		// Code that runs on entering the state.
+		public override void OnEnter()
+		{
+            FsmUpdate();
+			Finish();
+		}
+
+        void FsmUpdate()
+        {
+            QueueHub.AddJobToQueue(QueueName.Value, Fsm.Owner.gameObject, Update);
+        }
+
+        void Update()
+        {
+            Fsm.Update();
+        }
+
+
+	}
+
+}
